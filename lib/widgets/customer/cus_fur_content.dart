f623ad9d00bf1168c862d741fcb410fe638ee680
unknown <nqnhu6124@gmail.com>
2025-06-26 19:05:52 +0700

    app icon, link apk

--- lib/widgets/customer/cus_fur_content.dart
+++ lib/widgets/customer/cus_fur_content.dart
@@ -176,7 +176,7 @@ class _CusFurContentState extends State<CusFurContent> {
         crossAxisCount: 2,
         mainAxisSpacing: 16,
         crossAxisSpacing: 16,
-        childAspectRatio: 0.75,
+        childAspectRatio: 0.7,
       ),
       itemCount: _filteredDesigns.length,
       itemBuilder: (context, index) {
